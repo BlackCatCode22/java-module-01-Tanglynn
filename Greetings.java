75afb8dc86e918e0befa274d07151d28b22b648a
Chee Xiong <107579292+Cheex919@users.noreply.github.com>
2025-01-26 23:03:29 -0800

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,15 @@
+public class Greetings {
+}
+public static void main(String[] args) {
+}
+
+String usersName;
+String upperCaseName;
+
+system.out.print(Please enter your name: ");
+usersName = TextIO.getln();
+
+upperCaseName = usersName.toUpperCase();
+
+System.out.println("Hello, " + upperCaseName + ", nice to meet you.");
+}
