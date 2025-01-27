ba5559b3043396a2be5865e6f13ed5f804f289db
Chee Xiong <107579292+Cheex919@users.noreply.github.com>
2025-01-26 22:51:19 -0800

    Add files via upload

---
+++ VarandMath.java
@@ -0,0 +1,14 @@
+public class VarandMath {
+    public static void main(String[] args) {
+}
+
+int a = 4;
+
+double b = 2;
+
+double sum = a + b;
+
+System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
+
+
+
